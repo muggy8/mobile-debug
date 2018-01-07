129fcff0641e037e818564f482d88f14962e48b2
muggy8 <muggy8@gmail.com>
2018-01-07 00:58:59 -0800

    added some safety catches for no closing tag elements (like br) in create element representation

--- src/mobile-debug-elements.js
+++ src/mobile-debug-elements.js
@@ -6,13 +6,31 @@
 		</div>
 	`)
 	var createDomHtmlRepresentation = function(ele){
-		var clone = ele.cloneNode()
-		var nodeText = clone.outerHTML
-		var closingTag = nodeText.match(/<\/[^>]+>/)[0]
-		var oppeningTag = nodeText.replace(closingTag, "")
-
-		return {
-			close: closingTag,
-			open: oppeningTag
+		if (!ele){
+			return
 		}
+
+		if (ele instanceof HTMLElement){
+			var clone = ele.cloneNode()
+			var nodeText = clone.outerHTML
+			var closingTagMatch = nodeText.match(/<\/[^>]+>/)
+			if (!closingTagMatch){
+				return
+			}
+			var closingTag = closingTagMatch[0]
+			var oppeningTag = nodeText.replace(closingTag, "")
+			var childNodes = ele.childNodes
+
+			// debug and testing returns
+			return {
+				close: closingTag,
+				contents: ele.innerHTML,
+				open: oppeningTag
+			}
+		}
+		else if (ele instanceof Text){
+			systemLog = true;
+			return createDomStringRepresentation(ele.nodeValue)
+		}
+
 	}
